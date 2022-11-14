9f57cf8dd043258311457fcf4d9bab1b053a8864
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-11-14 19:13:00 +0100

    Dodanie przykładu SplitQuery

---
+++ src/EFCore6/SplitQuery/Context.cs
@@ -0,0 +1,16 @@
+﻿using SplitQuery.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SplitQuery
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+    }
+}
---
+++ src/EFCore6/SplitQuery/ContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SplitQuery
+{
+    internal class ContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            string connectionString = @"Server=(localdb)\mssqllocaldb;Database=SplitQueryDb";
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(connectionString).Options;
+
+            return new ApplicationDbContext(options);
+        }
+    }
+}
