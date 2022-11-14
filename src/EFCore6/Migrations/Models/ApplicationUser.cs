057c7d30c4e1a2ee13c7434f674dc6514db493aa
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-11-14 08:59:53 +0100

    ApplicationUser

--- src/EFCore6/Migrations/Models/ApplicationUser.cs
+++ src/EFCore6/Migrations/Models/ApplicationUser.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Migrations.Models
 {
-    internal class ApplicationUser
+    public abstract class BaseEntity
     {
+        public string Id { get; set; }
+    }
+
+
+    public class IdentityUser
+    {
+        public string Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
+        public string AccountNumber { get; set; }
     }
 }
