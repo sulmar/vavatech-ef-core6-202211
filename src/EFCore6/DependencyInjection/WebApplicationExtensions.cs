612d8b296bc7acc768977cc03d557541a25241f9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-12-01 10:55:16 +0100

    Utworzenie lub aktualizacja bazy danych na podstawie migracji

--- src/EFCore6/DependencyInjection/WebApplicationExtensions.cs
+++ src/EFCore6/DependencyInjection/WebApplicationExtensions.cs
@@ -4,6 +4,9 @@ namespace DependencyInjection
 {
     public static class WebApplicationExtensions
     {
+        /// <summary>
+        /// Utworzenie bazy danych bez migracji
+        /// </summary>
         public static WebApplication CreateDatabase<TContext>(this WebApplication app)
             where TContext : DbContext
         {
@@ -15,5 +18,22 @@ namespace DependencyInjection
 
             return app;
         }
+
+        /// <summary>
+        /// Utworzenie lub aktualizacja bazy danych na podstawie migracji
+        /// </summary>        
+        public static WebApplication CreateOrAlterDatabase<TContext>(this WebApplication app)
+           where TContext : DbContext
+        {
+            using var scope = app.Services.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<TContext>();
+
+            context.Database.Migrate();
+
+            return app;
+        }
+
+
     }
 }
